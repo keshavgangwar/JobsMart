51c7b75ffc02096733314e9857ce6912e2758e4c
Keshav Gangwar <gangwarkeshav3@gmail.com>
2025-03-15 03:13:22 +0530

    uploading of the various files and made changes into them

--- Backend/controller/ApplicationController.js
+++ Backend/controller/ApplicationController.js
@@ -1,5 +1,5 @@
-import { Application } from "../models/applicationModel";
-import { Job } from "../models/jobModel";
+import { Application } from "../models/applicationModel.js";
+import { Job } from "../models/jobModel.js";
 
 export const applyJob = async (req, res) => {
   try {
@@ -37,6 +37,90 @@ export const applyJob = async (req, res) => {
     });
     job.applications.push(newApplication._id);
     await job.save();
+    return res.status(201).json({
+      message: "Job applied Successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getAppliedJobs = async (req, res) => {
+  try {
+    const userId = req.id;
+    const applications = await Application.find({ applicant: userId })
+      .sort({
+        createdAt: -1,
+      })
+      .populate({
+        path: "job",
+        options: { sort: { createdAt: -1 } },
+        populate: {
+          path: "company",
+          options: { sort: { createdAt: -1 } },
+        },
+      });
+    if (!applications) {
+      return res.status(404).json({
+        message: "No applications found",
+        success: false,
+      });
+    }
+    return res.status(200).json({
+      applications,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getApplicants = async (req, res) => {
+  try {
+    const jobId = req.params.id;
+    const job = await Job.findById(jobId).populate({
+      path: "applications",
+      options: { sort: { createdAt: -1 } },
+      populate: {
+        path: "applicant",
+      },
+    });
+    if (!job) {
+      return res.status(404).json({
+        message: "Job not found",
+        success: false,
+      });
+    }
+    return res.status(200).json({
+      job,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const updateStatus = async (req, res) => {
+  try {
+    const applicationId = req.params.id;
+    const { status } = req.body;
+    if (!status) {
+      return res.status(400).json({
+        message: "Status is required",
+        success: false,
+      });
+    }
+    const application = await Application.findOne({ _id: applicationId });
+    if (!application) {
+      return res.status(404).json({
+        message: "Application not found",
+        success: false,
+      });
+    }
+    application.status = status;
+    await application.save();
+    return res.status(200).json({
+      message: "Application status updated successfully",
+      success: true,
+    });
   } catch (error) {
     console.log(error);
   }
--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,7 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/userRoutes.js";
 import companyRoute from "./routes/companyRoute.js";
 import jobRoute from "./routes/jobRoutes.js";
+import applicationRoute from "./routes/applicationRoute.js";
 
 dotenv.config({});
 
@@ -27,6 +28,7 @@ const port = process.env.port || 3000;
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 app.listen(port, () => {
   connectDB();
